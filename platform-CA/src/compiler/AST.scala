6f680149acbf5a02fd1d2300aee329398e3302b3
gruau <gruau@lri.fr>
2018-12-10 10:23:07 +0100

    virage du trait API

--- platform-CA/src/compiler/AST.scala
+++ platform-CA/src/compiler/AST.scala
@@ -7,6 +7,12 @@ import scala.language.higherKinds
 
 
 
+/**The 9 locus. Three simplicial locus: V for vertex, E for edge, F for face, */
+sealed abstract  class Locus
+class S extends Locus;  final class V extends S;  final class E extends S ;  final class F  extends S
+/** T stands for Transfer, and uses two simplicial locus. The first is the simplicial. T[V,E] corresponds to  eV  */
+final class T[S1<:S,S2<:S] extends Locus 
+
 
 /**used to compute a string encoding the locus, at compile time. */
 class repr[L <: Locus](val name: String)
@@ -24,8 +30,71 @@ object CentralSym{
 	implicit val vEv= new CentralSym[V,E,V];	implicit val fEf= new CentralSym[F,E,F];
 	implicit val vFe= new CentralSym[V,F,E];	implicit val eFv= new CentralSym[E,F,V];
 }
-
+class Red2[S1,S2,S3]
+object Red2{
+	implicit val VEF= new Red2[V,E,F];	 
+}
 object AST { 
+  
+ def displayableIn(l:LayerAST[_<:Locus,_<:Ring] ,f:AST[_<:Locus,_<:Ring]) = l.displayable(f)
+ def displayIn(l:LayerAST[_<:Locus,_<:Ring] ,f:AST[_<:Locus,_<:Ring]) = l.display(f)
+ def bugIfIn(l:LayerAST[_<:Locus,_<:Ring] ,f:AST[_<:Locus,B]) = l.bugIf(f)
+ //def layer[L<:Locus,R<:Ring](c:Circuit,nbit:Int )(implicit m : repr[L])=  LayerAST(c,nbit)
+//def setPred[L<:Locus,R<:Ring](l:LayerAST[L,R],f:AST[L,R]){l.setPred(f)}
+// def setNext[L<:Locus,R<:Ring](l:LayerAST[L,R],f:AST[L,R]){l.setNext(f)}
+   def const[L<:Locus,R<:Ring](c:Circuit ,cte:ASTB[R])(implicit m : repr[L])     = Const(c,cte)(m) ;
+ // def layer[L<:Locus,R<:Ring]( c:Circuit )(implicit m : repr[L])   =  Layer (c)(m)
+ def transfer[S1<:S,S2<:S,R<:Ring](arg : AST[T[S1,S2],R])(implicit m : repr[T[S2,S1]]) = Transfer(arg)(m)  ;
+ def sym[S1<:S,S2<:S,S3<:S, R<:Ring](arg : AST[T[S2,S1],R])(implicit m : repr[T[S2,S3]], t : CentralSym[S1,S2,S3]) = Sym(arg)(m,t)   ;
+ def v[S1<:S, R<:Ring](arg : AST[S1,R])(implicit m : repr[T[S1,V]],  m2 : repr[T[V,S1]])=Broadcast[S1,V,R](arg); // for broadcast, we want to specify only the direction where broadcasting takes place.
+	def e[S1<:S, R<:Ring](arg : AST[S1,R])(implicit m : repr[T[S1,E]],  m2 : repr[T[E,S1]])=Broadcast[S1,E,R](arg); // this is done using three function e,v,f. 
+	def f[S1<:S, R<:Ring](arg : AST[S1,R])(implicit m : repr[T[S1,F]],  m2 : repr[T[F,S1]])=Broadcast[S1,F,R](arg);
+	//def castB2R[L<:Locus,R<:I]( arg: AST[L,B] )(implicit m : repr[L])  = Unop[L,B,R] (castB2RN[R],arg );
+	def neg[L<:Locus, R<:Ring] ( arg : AST[L,R]) (implicit m : repr[L]) = Unop[L,R,R](negN[R],arg)   ;
+	def opp[L<:Locus ] ( arg : AST[L,SI]) (implicit m : repr[L]) = Unop[L,SI,SI](oppN,arg)   ;
+	def elem[L<:Locus, R<:I]   (i:Int , arg : AST[L,R]  ) (implicit m : repr[L]) = Unop[L,R,B](eltN[R](i),arg)   ;
+	def extend[L<:Locus, R<:I]   (i:Int , arg : AST[L,R]  ) (implicit m : repr[L]) = Unop[L,R,R](extendN[R](i),arg)   ;
+	def sign[L<:Locus] ( arg1 : AST[L,SI] )(implicit m : repr[L]) = Unop[L,SI,SI ](signN,arg1 );
+	def halve[L<:Locus, R<:I] ( arg1 : AST[L,R] )(implicit m : repr[L])  = Unop[L,R,R ](halveN,arg1 )
+  def orScanRight[L<:Locus, R<:I] ( arg1 : AST[L,R] )(implicit m : repr[L])    = Unop[L,R,R ](orScanRightN,arg1 )
+  def gt[L<:Locus] ( arg1 : AST[L,SI] )(implicit m : repr[L]) = Unop[L,SI,B ](gtN,arg1 );
+	//def binop [L<:Locus, R<:Ring] (implicit m : repr[L]) = Binop[L,R,R,R] _  ;
+	def orL[L<:Locus, R<:Ring]( arg1 : AST[L,R] , arg2 : AST[L,R])(implicit m : repr[L]) = Binop[L,R,R,R](orN,arg1,arg2 );
+	def andL[L<:Locus, R<:Ring]( arg1 : AST[L,R] , arg2 : AST[L,R]) (implicit m : repr[L]) = Binop[L,R,R,R](andN,arg1,arg2 );
+	def xorL[L<:Locus, R<:Ring]( arg1 : AST[L,R] , arg2 : AST[L,R]) (implicit m : repr[L]) = Binop[L,R,R,R](xorN,arg1,arg2 );
+	/** We avoid casting boolean to integer, instead we define operation taking an int and a  bool, and computing a new int, by mapping the and operation*/
+	def andLB2R [L<:Locus,R<:I]( arg1 : AST[L,B],arg2 : AST[L,R])(implicit m : repr[L]) = Binop[L,B,R,R]( andLB2RN,arg1 ,arg2);
+  def concat[L<:Locus,R<:I]( arg1 : Seq[AST[L,B]])(implicit m : repr[L])=  Multop[L,B,R](concatN, arg1 );
+	def addL[L<:Locus,R<:I] ( arg1 : AST[L,R], arg2 : AST[L,R])(implicit m : repr[L]):AST[L,R] = Binop[L,R,R,R](addN,arg1,arg2 ) ;
+	def orR[S1<:S,S2<:S, R<:Ring] (arg : AST[T[S1,S2],R])(implicit m : repr[S1]) = Redop[S1,S2,R] ((orN[R],False[R](arg.nbit)),arg );   
+	def xorR[S1<:S,S2<:S, R<:Ring] (arg : AST[T[S1,S2],R])(implicit m : repr[S1]) = Redop[S1,S2,R] ((xorN[R],False[R](arg.nbit)),arg );   
+  def xorR2[S1<:S,S2<:S,S2new<:S, R<:Ring] (arg : AST[T[S1,S2],R])(implicit m : repr[T[S1,S2new]]) = Redop2[S1,S2,S2new,R] ((xorN[R],False[R](arg.nbit)),arg );   
+	def minR[S1<:S,S2<:S,R<:I] (arg : AST[T[S1,S2],R])(implicit m : repr[S1]) = Redop[S1,S2,R] ((minN[R],ConstInt[R](0,1)),arg );
+	/** Delete uses a trick found on the net, to have a call by name, together with a case class necessary to make the match*/
+	def delayed[L<:Locus,R<:Ring](_arg: => AST[L,R],c:Circuit,nbit:Int )(implicit m : repr[L]): AST[L,R] = {
+  lazy val delayed = _arg  ;  Delayed(() => delayed,c,nbit) }
+  
+  
+  type IntV = AST[V,SI]; type IntE = AST[E,SI]; type IntF = AST[F,SI];
+	type IntvE = AST[T[E,V],SI]; type InteV = AST[T[V,E],SI];
+	type IntvF = AST[T[F,V],SI]; type IntfV = AST[T[V,F],SI];
+	type IntfE = AST[T[E,F],SI]; type InteF = AST[T[F,E],SI];
+  type UintV = AST[V,UI]; type UintE = AST[E,UI]; type UintF = AST[F,UI];
+	type UintvE = AST[T[E,V],UI]; type UinteV = AST[T[V,E],UI];
+	type UintvF = AST[T[F,V],UI]; type UintfV = AST[T[V,F],UI];
+	type UintfE = AST[T[E,F],UI]; type UinteF = AST[T[F,E],UI];  
+	type BoolV = AST[V,B]; type BoolE = AST[E,B]; type BoolF = AST[F,B];
+	type BooleV = AST[T[V,E],B]; type BoolvE = AST[T[E,V],B];
+	type BoolvF = AST[T[F,V],B]; type BoolfV = AST[T[V,F],B];
+	type BoolfE = AST[T[E,F],B]; type BooleF = AST[T[F,E],B];
+  def cond[L<:Locus,R<:I] (b:AST[L,B],  arg1 : AST[L,R] , arg2 : AST[L,R])(implicit m : repr[L])= orL(andLB2R[L,R](b,arg1),andLB2R(neg(b),arg2))
+  	/**  defined only for signed Int */
+  def minusL[L<:Locus] ( arg1 :  AST[L,SI], arg2 : AST[L,SI])(implicit m : repr[L]) = addL( arg1,opp(arg2)) ;
+  
+  def mstb[L<:Locus,R<:I] (arg1:AST[L,R])(implicit m : repr[L]): AST[L,R] = { val y:AST[L,R]= orScanRight[L,R](arg1); 
+    xorL(y,halve(y)) }  
+	
+	
 def toString3( a:AST[_,_]):String =  
  ( a   match {
 	case LayerAST(_,_) =>  "Layer"+a.locus
@@ -58,6 +127,11 @@ def toString3( a:AST[_,_]):String =
 	}
 }
 
+trait Named {
+  var name:String=null;  def setName(value: String) {name = value  };
+ def addAfter(value: String) {name =  name+value  };def addBefore(value: String) {name =  value+name  }
+}
+
 sealed abstract class AST[+L<:Locus,+R<:Ring]( val c :Circuit, val nbit:Int) (implicit m : repr[L]) extends Named with Bag { 
   /** not necessary, just to remember how to retrieve the name*/
   val locus:String = m.name 
@@ -72,12 +146,12 @@ sealed abstract class AST[+L<:Locus,+R<:Ring]( val c :Circuit, val nbit:Int) (im
  	override def toString:String=AST.toString3( this)
 } 
 /**stores a list of  ensure boolV and display in order to get name related to the layers.  */
- case class LayerAST[L<:Locus, R<:Ring](override val  c:Circuit,override val nbit:Int )(implicit m : repr[L]) extends AST[L,R](c,nbit) with EmptyBag{
+ abstract case class LayerAST[L<:Locus, R<:Ring](override val  c:Circuit,override val nbit:Int )(implicit m : repr[L]) extends AST[L,R](c,nbit) with EmptyBag{
    /** the value at t, which  is represented as  the layer itself.*/
    val pred:AST[L,R]=this;  
   /**value of the layer at t+1, mutable, since before computing the next value, we need to create other layers.  */
- var next:AST[L,R]=this;
-    def setNext(l:AST[L,R]){next=l};
+ val next:AST[L,R];
+   // def setNext(l:AST[L,R]){next=l};
   
 //for the following three lists, we must put ORs to make sure user is updated correctly.? 
   /** Boolean fields which must be true, otherwise bug is detected in layer.*/ 
--- platform-CA/src/prog/distance.scala
+++ platform-CA/src/prog/distance.scala
@@ -1,79 +1,48 @@
 package prog
-
 //import compiler.ASTB._
 //import compiler.AST._
 import compiler.Circuit 
-import compiler.LayerUsr
 import compiler.repr
-
-import compiler.API
-import compiler.APIstdlib
-import compiler.LanguageImpl 
+import compiler.AST._
 import compiler._
 import Compiler._
 
-  abstract   class  CycleLayer (c:Circuit, nbit:Int )extends  LayerUsr[V,SI] (c,nbit)   {
-  lazy val x:IntV=opp(opp(opp(opp(addL(next,pred ))))); 
-  val next=delayed(x,c,nbit); //préciser le circuit et le nombre de bit. 
-  setNext(pred,next);  
-}
-
-  abstract  class Program2 extends   APIstdlib {
-       
-  
-  val testCycle=new Circuit(){val chip= new CycleLayer(this,3)with  LanguageImpl  ;}
-    
-  
-  }
-abstract class Program extends   APIstdlib   {  
-  
- 
-  case class Dist(c:Circuit, val source:BoolV) extends LayerUsr[V,SI]  (c,3)  {
-    display(elt(2,pred ))
+  class Dist(c:Circuit, val source:BoolV) extends LayerAST[V,SI]  (c,3)  {
+    display(elem(2,this ))
    val grad:IntvE= minusL(transfer(e(pred)), transfer(e(pred)));
    val greater:BoolvE=gt(grad) 
    val greaterOptimized:BoolvE= andL(transfer(e(pred)),v(mstb(xorR(transfer(e(pred)))))) //same as greater, but cost in gates is diminished.
-   val vortex:BoolF= orR(transfer(xorR2 (transfer(greater))));   bugIf(vortex) 
+   val temp:BoolfV=xorR2(transfer(greater))
+   val vortex:BoolF= orR(transfer(temp));
+   bugIf(vortex) 
    val next=addL(pred,extend(3,cond(source, sign(opp(pred)), minR(transfer(sign(  addL(grad,const[T[E,V],SI](c,ConstInt(-2,3))))))))) 
-    setNext(pred,next)  
  }
   
-  val c1=new Circuit(){
-    val src = const[V,B](this,True[SI](1)); 
-    val dist = Dist(this,src);
-  } 
 
 /**returns a constant layer. */ 
-case class ConstLayer[L<:Locus, R<:Ring](c:Circuit, nbit:Int )(implicit m : repr[L]) extends LayerUsr[L,R]  (c,nbit) {
-  val next=pred;  setNext(pred,next);
+class ConstLayer[L<:Locus, R<:Ring](c:Circuit, nbit:Int )(implicit m : repr[L]) extends LayerAST[L,R]  (c,nbit) {
+  val next=this; 
 }
 
 
-  case class  CycleLayer (c:Circuit, nbit:Int )(implicit m : repr[V]) extends  LayerUsr[V,SI] (c,nbit)   {
+  class  CycleLayer (c:Circuit, nbit:Int )(implicit m : repr[V]) extends  LayerAST[V,SI] (c,nbit)   {
   lazy val x:IntV=opp(opp(opp(opp(addL(next,pred ))))); 
   val next=delayed(x,c,nbit); //préciser le circuit et le nombre de bit. 
-  setNext(pred,next);  
+  //setNext(pred,next);  
 }
-  
-  
-  val testCycle=new Circuit(){val chip= CycleLayer(this,3);}
-    
-}
+
+
 
 object Test { //  def g[L<:Locus](t:AST[L,B])(implicit m : repr[L]) = m.name; exemple de implicit que je conserve.
    def main(args: Array[String]) { 
-     
-//     val c1=new Circuit(){
-//      val source = Const[V,B](this,True()); 
-     //val dist = new Distance(this,source);
-     
-     
-      //println(test+" "+    test.s) 
-    //println(dd.next)
    
-     val p  = new Program()  with  LanguageImpl  
-     //compile(p.c1);     println(p.c1)
-     compile(p.testCycle);  //   println(p.testCycle)
+  val testDist=new Circuit(){
+    val src = const[V,B](this,True[SI](1)); 
+    val dist = new Dist(this,src);
+  } 
+  val testCycle=new Circuit(){val chip= new CycleLayer(this,3);}
+    
+     compile(testCycle);  //   println(p.testCycle)
    }
  
 }
